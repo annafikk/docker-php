d841f02b4db5643ed671061aaa6de9319466c549
Cnip! <107799812+annafikk@users.noreply.github.com>
2023-05-23 15:34:11 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -19,18 +19,9 @@
     </nav>
 
     <?php
-        //These are the defined authentication environment in the db service
-
-        // The MySQL service named in the docker-compose.yml.
         $host = 'PENDAFTARAN';
-
-        // Database use name
         $user = 'php_docker';
-
-        //database user password
         $pass = 'password';
-
-        // check the MySQL connection status
         $conn = new mysqli($host, $user, $pass);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
